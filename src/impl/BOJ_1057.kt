1dc3fbf6b859867641762546a9566058243762bc
henry <henry@lendit.co.kr>
2019-10-06 17:44:54 +0900

    add boj 1057

---
+++ src/impl/BOJ_1057.kt
@@ -0,0 +1,22 @@
+package impl
+
+class BOJ_1057 {
+    fun solve() {
+        var (n, kim, lim) = readLine()!!.split(" ").map { it.toInt() }
+
+        var ans = 0
+
+        while (kim != lim) {
+            kim = kim / 2 + kim % 2
+            lim = lim / 2 + lim % 2
+
+            ans++
+        }
+
+        println(ans)
+    }
+}
+
+fun main(args: Array<String>) {
+    BOJ_1057().solve()
+}
