c00f6b8bda6f61d59102838bd74b73701b02932f
henry <henry@lendit.co.kr>
2019-10-23 21:38:07 +0900

    add boj 1922

---
+++ src/dijkstra/BOJ_1916.kt
@@ -0,0 +1,12 @@
+package dijkstra
+
+class BOJ_1916 {
+    fun solve() {
+        val n = readLine()!!.toInt()
+        val m = readLine()!!.toInt()
+    }
+}
+
+fun main(args: Array<String>) {
+    BOJ_1916().solve()
+}
