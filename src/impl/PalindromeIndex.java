30803079d9dfacfe0acae1d946a11613d97a71ce
henry <henry@lendit.co.kr>
2019-12-11 22:46:08 +0900

    rename palindrome index

--- src/impl/PalindromeIndex.java
+++ src/impl/PalindromeIndex.java
@@ -2,7 +2,7 @@ package impl;
 
 import java.util.Scanner;
 
-public class Solution {
+public class PalindromeIndex {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
