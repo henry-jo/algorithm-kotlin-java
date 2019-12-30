87c45c65e96f96d8c94b3553d7aeb64d86919f36
henry <henry@lendit.co.kr>
2019-12-30 22:22:02 +0900

    add boj 1977

---
+++ src/impl/BOJ_1977.kt
@@ -0,0 +1,32 @@
+package impl
+
+import kotlin.math.min
+import kotlin.math.sqrt
+
+class BOJ_1977 {
+    fun solve() {
+        val m = readLine()!!.toInt()
+        val n = readLine()!!.toInt()
+
+        var sum = 0
+        var minVal = Int.MAX_VALUE
+
+        for(i in m .. n) {
+            if (sqrt(i.toDouble()) % 1.0f == 0.0) {
+                sum += i
+                minVal = min(i, minVal)
+            }
+        }
+
+        if (sum == 0) {
+            println(-1)
+        } else {
+            println(sum)
+            println(minVal)
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    BOJ_1977().solve()
+}
